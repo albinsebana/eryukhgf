e3b484c9c21f141c0b169c86179730afd63d9624
Albin Sebana Kurian <100872558+albinsebana@users.noreply.github.com>
2023-05-05 10:44:55 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:newsapp/screens/home.dart';
+
+
+void main() async {
+  runApp(NewsApp());
+  ByteData data = await PlatformAssetBundle().load('assets/ca/lets-encrypt-r3.pem');
+  SecurityContext.defaultContext.setTrustedCertificatesBytes(data.buffer.asUint8List());
+
+}
+
+class NewsApp extends StatelessWidget {
+  NewsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Copse'),
+      home: Home(),
+    );
+  }
+}
